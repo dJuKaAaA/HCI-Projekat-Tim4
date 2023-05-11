c3c1c313be6fc9566023ed1fba62aa459456b806
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2023-05-11 14:19:22 +0200

    Add ListView and DataGrid themes for flights

--- TravelAgent/TravelAgent/MVVM/ViewModel/MainViewModel.cs
+++ TravelAgent/TravelAgent/MVVM/ViewModel/MainViewModel.cs
@@ -4,20 +4,50 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TravelAgent.Core;
 using TravelAgent.Service;
 
 namespace TravelAgent.MVVM.ViewModel
 {
     public class MainViewModel : Core.ViewModel
     {
+        private Visibility _menuVisibility;
+
+        public Visibility MenuVisibility
+        {
+            get { return _menuVisibility; }
+            set { _menuVisibility = value; OnPropertyChanged(); }
+        }
+
         public NavigationService NavigationService { get; }
+
+        public ICommand OpenHelpCommand { get; }
+        public ICommand LogoutCommand { get; }
 
         public MainViewModel(
             NavigationService navigationService)
         {
             NavigationService = navigationService;
 
+            OpenHelpCommand = new RelayCommand(o => MessageBox.Show("Treba pomoc aaaa pickoooo"), o => true);
+            LogoutCommand = new RelayCommand(o => NavigationService.NavigateTo<LoginViewModel>(), o => true);
+
+            NavigationService.NavigationCompleted += (object sender, Type viewModelType) =>
+            {
+                if (viewModelType == typeof(LoginViewModel) || viewModelType == typeof(RegisterViewModel))
+                {
+                    MenuVisibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    MenuVisibility = Visibility.Visible;
+                }
+            };
+
             NavigationService.NavigateTo<AllFlightsViewModel>();
         }
     }
+
 }
