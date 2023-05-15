71e232ad654a13bf53f386f0022716eb88b06921
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2023-05-15 20:50:28 +0200

    Make initial sketch of PurchasedFlightsView

--- TravelAgent/TravelAgent/MVVM/ViewModel/PurchasedFlightsViewModel.cs
+++ TravelAgent/TravelAgent/MVVM/ViewModel/PurchasedFlightsViewModel.cs
@@ -13,5 +13,26 @@ namespace TravelAgent.MVVM.ViewModel
 {
     public class PurchasedFlightsViewModel : Core.ViewModel
     {
+        public ObservableCollection<FlightModel> AllFlights { get; set; }
+
+        private readonly Service.FlightService _flightService;
+
+        public PurchasedFlightsViewModel(
+            Service.FlightService flightService)
+        {
+            _flightService = flightService;
+
+            LoadAll();
+        }
+
+        private async void LoadAll()
+        {
+            AllFlights = new ObservableCollection<FlightModel>();
+            IEnumerable<FlightModel> flights = await _flightService.GetAll();
+            foreach (FlightModel flight in flights)
+            {
+                AllFlights.Add(flight);
+            }
+        }
     }
 }
