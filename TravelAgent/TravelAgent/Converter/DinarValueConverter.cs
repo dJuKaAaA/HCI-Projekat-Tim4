dabcbe4e6665fea17831b5c55de58bdf286b0b3b
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2023-05-18 11:33:13 +0200

    Add responsiveness to font sizes

--- TravelAgent/TravelAgent/Converter/DinarValueConverter.cs
+++ TravelAgent/TravelAgent/Converter/DinarValueConverter.cs
@@ -4,15 +4,17 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Markup;
 
-namespace TravelAgent.ValueConverter
+namespace TravelAgent.Converter
 {
-    public class HoursValueConverter : IValueConverter
+    public class DinarValueConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return $"{value}h";
+            return $"{value} RSD";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
---
+++ TravelAgent/TravelAgent/CustomComponent/ScalableTextBlock.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TravelAgent.CustomComponent
+{
+    public class ScalableTextBlock : TextBlock
+    {
+        private double _originalFontSize;
+
+        public ScalableTextBlock()
+        {
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _originalFontSize = FontSize;
+            Window window = Application.Current.MainWindow;
+            if (window != null)
+            {
+                window.SizeChanged += OnWindowSizeChanged;
+                UpdateFontSize(window.ActualWidth);
+            }
+        }
+
+        private void OnWindowSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (sender is Window window)
+            {
+                UpdateFontSize(window.ActualWidth);
+            }
+        }
+
+        private void UpdateFontSize(double windowWidth)
+        {
+            double maxWidth = SystemParameters.PrimaryScreenWidth;
+            double scaleFactor = windowWidth / maxWidth;
+            FontSize = _originalFontSize * scaleFactor;
+        }
+    }
+}
