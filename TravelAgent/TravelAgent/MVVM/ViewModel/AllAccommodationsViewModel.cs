b52245647512b70dc3f68659818ea89eda060179
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2023-05-19 13:14:40 +0200

    Give access to viewing trips to unregistered user

--- TravelAgent/TravelAgent/MVVM/ViewModel/AllAccommodationsViewModel.cs
+++ TravelAgent/TravelAgent/MVVM/ViewModel/AllAccommodationsViewModel.cs
@@ -23,7 +23,7 @@ namespace TravelAgent.MVVM.ViewModel
 
         public AllAccommodationsViewModel()
         {
-            ToolbarVisibility = MainViewModel.SignedUser.Type == Core.UserType.Traveler ? Visibility.Collapsed : Visibility.Visible;
+            ToolbarVisibility = MainViewModel.SignedUser?.Type == Core.UserType.Agent ? Visibility.Visible : Visibility.Collapsed;
 
             AllAccommodations = new ObservableCollection<AccommodationModel>()
             {
