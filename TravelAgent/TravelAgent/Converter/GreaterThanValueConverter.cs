b2d435f7d5d782d63a72ee91329e40eeadc26b4f
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2023-05-21 13:35:57 +0200

    Make AllTripsView responsive

---
+++ TravelAgent/TravelAgent/Converter/GreaterThanValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TravelAgent.Converter
+{
+    public class GreaterThanValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return double.TryParse(value.ToString(), out double doubleValue) &&
+                double.TryParse(parameter.ToString(), out double doubleParamter) &&
+                doubleValue > doubleParamter;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
