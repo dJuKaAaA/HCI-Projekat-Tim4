d0335ef41b67d51b3eb3bd783ec30137c97caf52
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2023-05-24 15:49:05 +0200

    Add drag and drop and restoraunts style
    Points of interest can now be picked by dragging from the list view that
    contains all of them into the one that the trip should contain. Also,
    the restoraunt drag and drop list views now have a style.

--- TravelAgent/TravelAgent/MVVM/View/Popup/SeeDealPopup.xaml.cs
+++ TravelAgent/TravelAgent/MVVM/View/Popup/SeeDealPopup.xaml.cs
@@ -24,6 +24,9 @@ namespace TravelAgent.MVVM.View.Popup
     /// </summary>
     public partial class SeeDealPopup : Window
     {
+
+        private SeeDealViewModel _viewModel;
+
         public SeeDealPopup(
             TripModel trip,
             UserTripService userTripService,
@@ -34,73 +37,28 @@ namespace TravelAgent.MVVM.View.Popup
             Consts consts)
         {
             InitializeComponent();
+            _viewModel = ((SeeDealViewModel)DataContext);
 
-            ((SeeDealViewModel)DataContext).Trip = trip;
-            ((SeeDealViewModel)DataContext).UserTripService = userTripService;
-            ((SeeDealViewModel)DataContext).MapService = mapService;
-            ((SeeDealViewModel)DataContext).TouristAttractionService = touristAttractionService;
-            ((SeeDealViewModel)DataContext).RestorauntService = restorauntService;
-            ((SeeDealViewModel)DataContext).AccommodationsService = accommodationService;
-            ((SeeDealViewModel)DataContext).Consts = consts;
-            DateTime takeoff = ((SeeDealViewModel)DataContext).Trip.DepartureDateTime;
-            DateTime landing = ((SeeDealViewModel)DataContext).Trip.ArrivalDateTime;
+            _viewModel.Trip = trip;
+            _viewModel.UserTripService = userTripService;
+            _viewModel.MapService = mapService;
+            _viewModel.TouristAttractionService = touristAttractionService;
+            _viewModel.RestorauntService = restorauntService;
+            _viewModel.AccommodationsService = accommodationService;
+            _viewModel.Consts = consts;
+            DateTime takeoff = _viewModel.Trip.DepartureDateTime;
+            DateTime landing = _viewModel.Trip.ArrivalDateTime;
             TimeSpan timeDiff = landing - takeoff;
-            ((SeeDealViewModel)DataContext).TripDuration = timeDiff.Hours;
-        }
-
-        private async void DrawPointsOfInterestPushpins()
-        {
-            MapService mapService = ((SeeDealViewModel)DataContext).MapService;
-            Consts consts = ((SeeDealViewModel)DataContext).Consts;
-            // TODO: Add special icons for each type of point of interest
-
-            // drawing tourist attractions
-            await ((SeeDealViewModel)DataContext).LoadTouristAttractionsForTrip();
-            foreach (TouristAttractionModel touristAttraction in ((SeeDealViewModel)DataContext).TouristAttractionsForTrip)
-            {
-                Pushpin touristAttractionPushpin = mapService.CreatePushpin(
-                    touristAttraction.Location.Latitude,
-                    touristAttraction.Location.Longitude,
-                    touristAttraction.Name,
-                    $"TouristAttraction_{touristAttraction.Id}",
-                    $"{consts.PathToIcons}/{consts.TouristAttractionPushpin}");
-                mapControl.Children.Add(touristAttractionPushpin);
-            }
-
-            // TODO: Draw restoraunts
-            await ((SeeDealViewModel)DataContext).LoadRestorauntsForTrip();
-            foreach (RestorauntModel restoraunt in ((SeeDealViewModel)DataContext).RestorauntsForTrip)
-            {
-                Pushpin restorauntPushpin = mapService.CreatePushpin(
-                    restoraunt.Location.Latitude,
-                    restoraunt.Location.Longitude,
-                    restoraunt.Name,
-                    $"Restoraunt_{restoraunt.Id}",
-                    $"{consts.PathToIcons}/{consts.RestorauntPushpin}");
-                mapControl.Children.Add(restorauntPushpin);
-            }
-
-            // TODO: Draw accommodations
-            await ((SeeDealViewModel)DataContext).LoadAccommodationsForTrip();
-            foreach (AccommodationModel accommodation in ((SeeDealViewModel)DataContext).AccommodationsForTrip)
-            {
-                Pushpin accommodationPushpin = mapService.CreatePushpin(
-                    accommodation.Location.Latitude,
-                    accommodation.Location.Longitude,
-                    accommodation.Name,
-                    $"Restoraunt_{accommodation.Id}",
-                    $"{consts.PathToIcons}/{consts.AccommodationPushpin}");
-                mapControl.Children.Add(accommodationPushpin);
-            }
+            _viewModel.TripDuration = timeDiff.Hours;
         }
 
         private void Popup_Loaded(object sender, RoutedEventArgs e)
         {
             DrawPointsOfInterestPushpins();
 
-            MapService mapService = ((SeeDealViewModel)DataContext).MapService;
+            MapService mapService = _viewModel.MapService;
 
-            TripModel trip = ((SeeDealViewModel)DataContext).Trip;
+            TripModel trip = _viewModel.Trip;
 
             Pushpin departurePushpin = mapService.CreatePushpin(
                 trip.Departure.Latitude,
@@ -122,5 +80,49 @@ namespace TravelAgent.MVVM.View.Popup
             mapControl.Center = departurePushpin.Location;
         }
 
+        private async void DrawPointsOfInterestPushpins()
+        {
+            MapService mapService = _viewModel.MapService;
+            Consts consts = _viewModel.Consts;
+
+            // Draw tourist attractions
+            await _viewModel.LoadTouristAttractionsForTrip();
+            foreach (TouristAttractionModel touristAttraction in _viewModel.TouristAttractionsForTrip)
+            {
+                Pushpin touristAttractionPushpin = mapService.CreatePushpin(
+                    touristAttraction.Location.Latitude,
+                    touristAttraction.Location.Longitude,
+                    touristAttraction.Name,
+                    $"TouristAttraction_{touristAttraction.Id}",
+                    $"{consts.PathToIcons}/{consts.TouristAttractionPushpinIcon}");
+                mapControl.Children.Add(touristAttractionPushpin);
+            }
+
+            // Draw restoraunt
+            await _viewModel.LoadRestorauntsForTrip();
+            foreach (RestorauntModel restoraunt in _viewModel.RestorauntsForTrip)
+            {
+                Pushpin restorauntPushpin = mapService.CreatePushpin(
+                    restoraunt.Location.Latitude,
+                    restoraunt.Location.Longitude,
+                    restoraunt.Name,
+                    $"Restoraunt_{restoraunt.Id}",
+                    $"{consts.PathToIcons}/{consts.RestorauntPushpinIcon}");
+                mapControl.Children.Add(restorauntPushpin);
+            }
+
+            // Draw accommodations
+            await _viewModel.LoadAccommodationsForTrip();
+            foreach (AccommodationModel accommodation in _viewModel.AccommodationsForTrip)
+            {
+                Pushpin accommodationPushpin = mapService.CreatePushpin(
+                    accommodation.Location.Latitude,
+                    accommodation.Location.Longitude,
+                    accommodation.Name,
+                    $"Restoraunt_{accommodation.Id}",
+                    $"{consts.PathToIcons}/{consts.AccommodationPushpinIcon}");
+                mapControl.Children.Add(accommodationPushpin);
+            }
+        }
     }
 }
