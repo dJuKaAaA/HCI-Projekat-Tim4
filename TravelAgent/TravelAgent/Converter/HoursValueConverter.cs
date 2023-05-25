25d0e1e0e74c80d645fd6c7a09fe13175405e14f
Ivan Djukanovic <ivan.djukanovic07@gmail.com>
2023-05-25 15:59:52 +0200

    Fix trip duration display

--- TravelAgent/TravelAgent/Converter/HoursValueConverter.cs
+++ TravelAgent/TravelAgent/Converter/HoursValueConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,20 @@ using System.Windows.Data;
 
 namespace TravelAgent.Converter
 {
+    // takes milliseconds and converts it into format "HH hours, mm minutes"
     public class HoursValueConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return $"{value}h";
+            int millis = (int)value;
+            int hours = millis / (1000 * 60 * 60);
+            millis -= hours * 1000 * 60 * 60;
+            int minutes = millis / (1000 * 60);
+            if (minutes == 0)
+            {
+                return $"{hours}h";
+            }
+            return $"{hours}h and {minutes}min";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
